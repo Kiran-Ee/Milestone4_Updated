7aea1124f20609cdf01173137abd8f819b6cb61d
Kiran-Ee <earnsha2@uwm.edu>
2024-04-26 15:27:03 -0500

    4/26/24 - KE - Tanja Instructions - LOOK AT "AddTest" - "GetRegistersStateTest"'s "@BeforeEach" method has already set every CPU value for your to adjust/copy and paste in your tests.

--- Tests/Milestone4Tests/EX_Operations/AddTest.java
+++ Tests/Milestone4Tests/EX_Operations/AddTest.java
@@ -8,29 +8,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddTest {
-    Add add = null;
-//    String a0 = "$a0";  // we're not sending in "registers" to OpObjs anymore ... we're sending 32 bit Bin (so we don't have to change MLST3)
-//    String a1 = "$a1";
-//    String a2 = "$a2";
+    Add add = null; // used to instantiate new OperationObj every test ...
 
-    int a0_val = 4;
+    int a0_val = 4; // used to perform operation ...
     int a1_val = 10;
     int a2_val = 15;
 
     int exp_add1 = 14;
+    // add more return variables for more tests ...
 
     @BeforeEach
-    public void setUp() { // setting the CPU ... I would test for a few more random registers (it's up to you if you want to test all of them)
+    public void setUp() { // setting the CPU:
         CPU.a0 = a0_val;
         CPU.a1 = a1_val;
         CPU.a2 = a2_val;
+        // set more regs ... LOOK AT "GetRegistersStateTest"'s "@BeforeEach" method for template!
     }
 
     @Test
     public void setAdd1(){
-        add = new Add("00000000101001100010000000100000");
+        add = new Add("00000000101001100010000000100000"); // we're not sending in "registers" to OpObjs ... we're sending 32 bit Bin (so we don't have to change MLST3)
         add.operate();
         assertEquals(CPU.a2 , exp_add1);
     }
 
+    // add more combos (testing different ints & registers) ...
 }
