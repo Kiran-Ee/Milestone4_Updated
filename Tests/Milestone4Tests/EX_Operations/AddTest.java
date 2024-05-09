2559370c5bd6bfa13ff84f0bb979480c61567269
Kiran-Ee <earnsha2@uwm.edu>
2024-05-09 01:27:10 -0500

    5/8/24 - KE - Passing Add - ^ fixed  + added tests.

--- Tests/Milestone4Tests/EX_Operations/AddTest.java
+++ Tests/Milestone4Tests/EX_Operations/AddTest.java
@@ -13,6 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 // machine-code: sp, rs, rt, rd, 0, add
 public class AddTest {
     Add add = null; // used to instantiate new OperationObj every test ...
+    String bin_instr1 = "00000000101001100010000000100000"; //ADD $a0 $a1 $a2
+    String bin_inst_2 = "00000010001011111000000000100000"; //ADD $s0 $s1 $t7
+    String bin_inst_3 = "00000000000000000000000000100000"; //add zero zero zero
+    String bin_inst_4 = "00000011000000101001100000100000"; //add s3 t8 v0
+
 
     int a0_val = 4; // used to perform operation ...
     int a1_val = 10;
@@ -20,9 +25,15 @@ public class AddTest {
     int s0_val = 16;
     int s1_val = 17;
     int t7_val = 15;
-    int exp_add1 = 14;
-    int exp_add2 = 20;
-    // add more return variables for more tests ...
+    int zero_val = 0;
+    int v0_val = 65535; //max unsigned
+    int t8_val = -1;
+
+
+    int exp_add1 = 15;
+    int exp_add2 = 32;
+    int exp_add3 = 0;
+    int exp_add4 = 65534;
 
     @BeforeEach
     public void setUp() { // setting the CPU:
@@ -32,28 +43,33 @@ public class AddTest {
         CPU.s0 = s0_val;
         CPU.s1 = s1_val;
         CPU.t7 = t7_val;
-        // set more regs ... LOOK AT "GetRegistersStateTest"'s "@BeforeEach" method for template!
+        CPU.zero = zero_val;
+        CPU.v0 = v0_val;
+        CPU.t8 = t8_val;
     }
-//String bin_instr1 = "00000010001011111000000000100000"; // "add", "$s0", "$s1", "$t7"
     @Test
     public void setAdd1(){
-        add = new Add("00000000101001100010000000100000"); // we're not sending in "registers" to OpObjs ... we're sending 32 bit Bin (so we don't have to change MLST3)
+        add = new Add(bin_instr1); // we're not sending in "registers" to OpObjs ... we're sending 32 bit Bin (so we don't have to change MLST3)
         add.operate();
         assertEquals(CPU.a2 , exp_add1);
     }
     @Test
     public void setAdd2(){
-        add = new Add("00000010001011111000000000100000");
+        add = new Add(bin_inst_2);
         add.operate();
-        assertEquals(CPU.t7, exp_add2);
+        assertEquals(CPU.s0, exp_add2);
     }
     @Test
-    public void setAdd5(){
-        // Test adding zero to a register
-        add = new Add("00000000000000000000000000000000");
+    public void setAdd3(){
+        add = new Add(bin_inst_3);
         add.operate();
-        assertEquals(CPU.a2, a2_val); // Expected result for the new test case should be the initial value?
+        assertEquals(CPU.zero, exp_add3);
     }
 
-    // add more combos (testing different ints & registers) ...
+    @Test
+    public void setAdd4(){
+        add = new Add(bin_inst_4);
+        add.operate();
+        assertEquals(exp_add4, CPU.s3); // Expected result for the new test case should be the initial value?
+    }
 }
