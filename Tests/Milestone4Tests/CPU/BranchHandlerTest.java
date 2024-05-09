20cc53c33ab845734e119b327fb2561c28385ed1
Kiran-Ee <earnsha2@uwm.edu>
2024-05-09 02:41:38 -0500

    5/9/24 - KE - Passing Beq - ^ fixed  + added tests.

---
+++ Tests/Milestone4Tests/CPU/BranchHandlerTest.java
@@ -0,0 +1,12 @@
+package Milestone4Tests.CPU;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BranchHandlerTest {
+
+    @Test
+    void branch_handler() {
+    }
+}
--- Tests/Milestone4Tests/EX_Operations/BeqTest.java
+++ Tests/Milestone4Tests/EX_Operations/BeqTest.java
@@ -5,35 +5,56 @@ import Operations.Beq;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static CPU.CPU.branch_handler;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //op-code-BEQ, rs, rt, offset
 //Machine-BEQ, rs, rt, offset
 public class BeqTest {
     Beq beq = null;
-    String bin_instr1 = "00010001101000100000000000000000"; //beq t5 v0 (0)-0x0000
-    int t5_val = 13 ;
+    String bin_instr1 = "00010001101000100000000000000000"; //beq t5 v0 0x0000
+    String bin_instr2 = "00010001101011010000000011111111"; //beq t5 t5 0xff
+
+    int t5_val = 13;
     int v0_val = 2;
-    //int exp_val1 = 0; //Expected PC value if branch is taken
-    //136affae beq {opcode: 04, rs(base): 1b, rt: 0a, immediate(offset): ffae}
+
+    String exp_val1 = "no branch";
+    String exp_val2 = "branch";
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         CPU.t5 = t5_val;
         CPU.v0 = v0_val;
-
     }
 
     @Test
-    public void setBeq(){
+    public void setBeq1() {
         beq = new Beq(bin_instr1);
-        beq.operate(); //opereate need to se if registers are equal
-        // Check if the PC has changed indirectly by the effect of the branch - if == adjust program counter -->
-        //assertTrue(someEffectOfBranchTakenOccurred());
-
+        assertEquals(exp_val1, beq.operate());
     }
-    //may need multiple Helper method to check some effect of branch taken or not taken
-//    private boolean someEffectOfBranchTakenOccurred() {
-//        // maybe to check if a specific register or variable has changed as a result of the branch or overall program changed
-//        return (variableChange || registerChange);
-//    }
+
+    @Test
+    public void setBeq2() {
+        beq = new Beq(bin_instr2);
+        assertEquals(exp_val2, beq.operate());
+    }
+
+    // Testing branch_hanlder
+
+    @Test
+    public void branchHandler_Beq1() {
+        beq = new Beq(bin_instr1);
+        int pc1 = 2;
+        int exp_pc1 = 2;
+        assertEquals(exp_pc1, branch_handler(beq,pc1));
+    }
+
+    @Test
+    public void branchHandler_Beq2() {
+        beq = new Beq(bin_instr2);
+        int pc2 = 2;
+        int exp_pc2 = pc2 + 1 + 255;
+        assertEquals(exp_pc2, branch_handler(beq,pc2));
+    }
 }
