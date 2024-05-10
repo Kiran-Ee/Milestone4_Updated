c0b13d756156d6f07192410c6245ec26f2f75d8f
Tanja <tcapps@uwm.edu>
2024-05-09 19:23:38 -0500

    testPush

---
+++ Tests/Operations/deteketasdf.java
@@ -0,0 +1,13 @@
+package Operations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class deteketasdf {
+
+    @Test
+    void binary_parser() {
+        int a = 0;
+    }
+}
