07c5b3bfd61d1ba985d2cabe6dd0932e50fdeb2c
Kiran-Ee <earnsha2@uwm.edu>
2024-05-15 18:23:29 -0500

    5/15/24 - KE - Passing LwTest - ^

--- Tests/Milestone4Tests/EX_Operations/LwTest.java
+++ Tests/Milestone4Tests/EX_Operations/LwTest.java
@@ -13,57 +13,127 @@ import static org.junit.Assert.assertThrows;
 
 public class LwTest {
     Lw lw = null;
-    LinkedHashMap<String, String> hm1 = new LinkedHashMap<>();
+    LinkedHashMap<String, Integer> hm1 = new LinkedHashMap<>();
 
+    String bin_instr1 = "10001110001110100000000000000000"; //lw k0 0x0 s1 <- testing no offset
+    String bin_instr2 = "10001110001010000000000000001000"; //"lw", "$t0", "8", "$s1 <- testing offset
+    String bin_instr3 = "10001110001111011111111111111100";// lw sp -4(s1) <- testing negative offset
+    String bin_invalid1 = "10001110001010000000000000001010"; //"lw", "$t0", "10", "$s1 ... offset must be factor of 4 bc word addressing (4 bytes)
 
-    String bin_instr1 = "10001110001010000000000000001000"; //"lw", "$t0", "8", "$s1
-    String bin_instr2 = "10001110001110100000000000000000"; //lw k0 0x0 s1
-    String bin_invalid_instr1 = "10001110001010000000000000001010"; //"lw", "$t0", "10", "$s1 ... offset must be factor of 4 bc word addressing (4 bytes)
-
-    int t0_val = 8;
-    int s1_val = 268500992; //0x10010000
-
+    // Dstination Registers
     int k0_val = 938457;
-    //int s1_val = 268500992; //0x10010000
+    int t0_val = 8;
+    int sp_val = 0xffffffff;
 
+    // Base Register ($s1) Value ... not exact address of the starting addresses bc not word aligned
+    int s1_val1 = 268500992; //0x10010000 - use w/ instr1 and instr2
+    int s1_val2 = 268501012; //0x10010014 - use w/ instr1 and instr2 and instr3
+    int s1_val3 = 268501032; //0x10010028 - use w/ instr1 and instr3
 
-    int exp_lui1 = 0x69207275;
-    int exp_lui2 = 0x65746e45;
-    // exception for invalid1
+    // Expected Address
+    int exp_instr1_val1 = 0x65746e45; //0x10010000 0 offset
+    int exp_instr2_val1 = 0x69207275; //0x10010000 8 offset
 
+    int exp_instr1_val2 = 0x756f5900; //0x10010014 0 offset
+    int exp_instr2_val2 = 0x65676574; //0x10010014 8 offset
+    int exp_instr3_val2 = 0x203a7265; //0x10010014 -4 offset
 
+    int exp_instr1_val3 = 0x5900214e; //0x10010028 0 offset
+    int exp_instr3_val3 = 0x45564520; //0x10010028 -4 offset
 
     @BeforeEach
     public void setUp() {
         CPU.t0 = t0_val;
-        CPU.s1 = s1_val;
-
         CPU.k0 = k0_val;
+        CPU.sp = sp_val;
 
-        // TODO
-//        DataSecConverter.data_mem.put("10010000", "Enter your integer: ");
-//        DataSecConverter.data_mem.put("10010015", "Your integer is EVEN!");
-//        DataSecConverter.data_mem.put("1001002b", "Your integer is ODD!");
+        hm1.put("10010000", 0x65746e45);
+        hm1.put("10010004", 0x6f792072);
+        hm1.put("10010008", 0x69207275);
+        hm1.put("1001000c", 0x6765746e);
+        hm1.put("10010010", 0x203a7265);
+        hm1.put("10010014", 0x756f5900);
+        hm1.put("10010018", 0x6e692072);
+        hm1.put("1001001c", 0x65676574);
+        hm1.put("10010020", 0x73692072);
+        hm1.put("10010024", 0x45564520);
+        hm1.put("10010028", 0x5900214e);
+        hm1.put("1001002c", 0x2072756f);
+        hm1.put("10010030", 0x65746e69);
+        hm1.put("10010034", 0x20726567);
+        hm1.put("10010038", 0x4f207369);
+        hm1.put("1001003c", 0x00214444);
+
+        DataSecConverter.data_mem = hm1;
     }
 
     @Test
-    public void setLui1() {
+    public void instr1_val1() {
         lw = new Lw(bin_instr1);
+        CPU.s1 = s1_val1;
+
         lw.operate();
-        assertEquals(exp_lui1, CPU.t0);
+        assertEquals(exp_instr1_val1, CPU.k0);
     }
 
     @Test
-    public void setLui2() {
+    public void instr2_val1() {
         lw = new Lw(bin_instr2);
+        CPU.s1 = s1_val1;
+
         lw.operate();
-        assertEquals(exp_lui2, CPU.k0);
+        assertEquals(exp_instr2_val1, CPU.t0);
     }
 
     @Test
-    public void setLui1_invalid() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            lw = new Lw(bin_invalid_instr1);
-        });
+    public void instr1_val2() {
+        lw = new Lw(bin_instr1);
+        CPU.s1 = s1_val2;
+
+        lw.operate();
+        assertEquals(exp_instr1_val2, CPU.k0);
     }
+
+    @Test
+    public void instr2_val2() {
+        lw = new Lw(bin_instr2);
+        CPU.s1 = s1_val2;
+
+        lw.operate();
+        assertEquals(exp_instr2_val2, CPU.t0);
+    }
+
+    @Test
+    public void instr3_val2() {
+        lw = new Lw(bin_instr3);
+        CPU.s1 = s1_val2;
+
+        lw.operate();
+        assertEquals(exp_instr3_val2, CPU.sp);
+    }
+
+    @Test
+    public void instr1_val3() {
+        lw = new Lw(bin_instr1);
+        CPU.s1 = s1_val3;
+
+        lw.operate();
+        assertEquals(exp_instr1_val3, CPU.k0);
+    }
+
+    @Test
+    public void instr3_val3() {
+        lw = new Lw(bin_instr3);
+        CPU.s1 = s1_val3;
+
+        lw.operate();
+        assertEquals(exp_instr3_val3, CPU.sp);
+    }
+
+//    @Test <- I don't think it's worth to add this to constructor because could throw exceptions in unexpedicted situations
+//    public void invalid1() {
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            lw = new Lw(bin_invalid1);
+//        });
+//    }
 }
--- src/SecConverters/DataSecConverter.java
+++ src/SecConverters/DataSecConverter.java
@@ -45,6 +45,7 @@ public class DataSecConverter {
 
     /**
      * Looks for the address in memory, starts piecing together (undoing little endian) the label until reaching null byte.
+     * Used to extract strings from memory.
      * @param key_address
      * @return the corresponding label
      */
@@ -83,6 +84,19 @@ public class DataSecConverter {
         }
         return lbl;
     }
+}
+
+
+
+
+    /* outdated data_mem ...
+    {“address in memory” : “data” }
+        {“10010000” : "Enter your integer: "}
+        {“10010015” : "Your integer is EVEN!"}
+        {“1001002b” : "Your integer is ODD!"}
+     */
+
+/*
 
     // Converts a hex_string in little endian to its original string
     // Ex:"65746e45" -> Ente
@@ -134,14 +148,4 @@ public class DataSecConverter {
         int sum = hexValue + current_size;
         return Integer.toHexString(sum);
     }
-}
-
-
-
-
-    /* outdated data_mem ...
-    {“address in memory” : “data” }
-        {“10010000” : "Enter your integer: "}
-        {“10010015” : "Your integer is EVEN!"}
-        {“1001002b” : "Your integer is ODD!"}
-     */
+ */
